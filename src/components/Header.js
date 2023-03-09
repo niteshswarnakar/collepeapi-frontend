abff12bbf491b68f1e6bcdf018ed0f9a43d496d4
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2023-03-09 22:42:40 +0545

    modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,30 @@ import React from "react";
 import mylogo from "../assets/webpagelogo.png";
 import { Link } from "react-router-dom";
 import classes from "../styles/Header.module.css";
+import iamnitesh from "../assets/iamnitesh.png";
+import { SocialIcon } from "react-social-icons";
 const Header = () => {
   return (
     <div className={classes.headerdiv}>
       <div className={classes.header}>
         <div className={classes.left}>
-          <img className={classes.mylogo} src={mylogo} />
+          <a href="/" className={classes.topHome}>
+            <img className={classes.mylogo} src={mylogo} />
+            <span>Home</span>
+          </a>
         </div>
         <div className={classes.right}>
-          <Link style={{ textDecoration: "none", color: "#000" }} to="/">
-            @neptechbyte
-          </Link>
-          <Link style={{ textDecoration: "none", color: "#000" }} to="/">
-            .
-          </Link>
+          <a
+            className={classes.youtubeIcon}
+            style={{ textDecoration: "none", color: "#000" }}
+            to="https://www.youtube.com/@neptechbyte/videos">
+            <SocialIcon url="https://www.youtube.com/@neptechbyte/videos" />
+          </a>
+          <a
+            style={{ textDecoration: "none", color: "#000" }}
+            href="https://www.youtube.com/@neptechbyte/videos">
+            neptech
+          </a>
         </div>
       </div>
     </div>
