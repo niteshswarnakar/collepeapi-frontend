35e4dcc42d78af41f0ff28687340d3edde2ef847
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2023-03-10 13:17:20 +0545

    downtime

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Header from "./components/Header";
 import Signup from "./pages/Signup";
 import Button from "./components/Button";
 import ElectionPost from "./components/ElectionPost";
+import classes from "./App.module.css";
 function App() {
   return (
     <Router>
       <Header />
-      <ElectionPost />
-      <Routes>
+      <div className={classes.ErrorMessage}>
+        <h1>Kei samaye lai banda gariyeko xa 👺...</h1>
+      </div>
+      {/* <ElectionPost /> */}
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
-      </Routes>
+      </Routes> */}
     </Router>
   );
 }
