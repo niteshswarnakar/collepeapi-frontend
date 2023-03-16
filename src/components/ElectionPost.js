48224348da9e5d1ddcea73e728f42ded82721aca
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2023-03-16 13:20:19 +0545

    feat: add meta image of pulchowk girls

--- src/components/ElectionPost.js
+++ src/components/ElectionPost.js
@@ -1,12 +1,13 @@
 import React from "react";
-import electionFlex from "../assets/architecture.jpg";
+// import electionFlex from "../assets/architecture.jpg";
+import pulchowkGirls from "../assets/pulchowkgirls.jpg";
 import classes from "../App.module.css";
 const ElectionPost = () => {
   return (
     <div className={classes.electionContainer}>
       <marquee>
-        {/* <img className={classes.electionFlex} src={electionFlex}></img>; */}
-        <h2>Choose your candidate responsibly</h2>
+        <img className={classes.electionFlex} src={pulchowkGirls}></img>;
+        {/* <h2>Choose your candidate responsibly</h2> */}
       </marquee>
     </div>
   );
