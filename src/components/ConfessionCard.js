6fc10cd04ab144001d85323a8d507d5d4a187520
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2023-06-08 19:54:20 +0545

    hotfix: corrected To and From label in confession card

--- src/components/ConfessionCard.js
+++ src/components/ConfessionCard.js
@@ -4,8 +4,8 @@ const ConfessionCard = ({ allConfession, confession }) => {
   return (
     <div className={classes.cardBox}>
       <div>
-        <p>To: {confession.from}</p>
-        <p className={classes.toField}>From: {confession.to}</p>
+        <p>From: {confession.from}</p>
+        <p className={classes.toField}>To: {confession.to}</p>
       </div>
       <p className={classes.message}>{confession.message}</p>
       <p className={classes.date}>{confession.date}</p>
